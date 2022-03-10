f441bb24eb7496189f6d40bc0b40e870d71ea44e
Your Name <you@example.com>
2022-03-10 14:39:22 +0300

    final?

--- js/changePrice.js
+++ js/changePrice.js
@@ -1,10 +1,9 @@
-let buttons = document.querySelectorAll(".btn-add-to-cart");
-let backUp = document.querySelectorAll(".btn-add-to-cart");
+let buttons = document.getElementsByClassName("btn-add-to-cart");
 let prices = document.getElementsByClassName("item-price");
 
-cnt1 = 0;
-
 let values = [];
+
+var cnt = 0;
 
 for (let val of buttons) {
     values[cnt] = val.innerHTML;
@@ -12,33 +11,26 @@ for (let val of buttons) {
 }
 
 if (window.innerWidth <= 320) {
-    var cnt = 0;
+    cnt = 0;
     for (let btn of buttons) {
         btn.innerHTML = prices[cnt].innerHTML;
         cnt++;
     }
 }
+
 window.addEventListener("resize", function(){  
-    console.log(backUp);
-    var backcnt = 0;
+    cnt = 0;
     if (window.innerWidth <= 320) {
         for (let btn of buttons) {
-            // console.log(btn.innerHTML);
-            btn.innerHTML = prices[backcnt].innerHTML;
+            btn.innerHTML = prices[cnt].innerHTML;
             cnt++;
         }
     }
-    else {
-        backcnt = 0;
-        if (window.innerWidth > 320) {
-            console.log("back");
-            console.log(backUp);
-            for (let btn of buttons) {
-               // console.log(btn);
-              //  console.log(backUp[backcnt].innerHTML);
-                btn.innerHTML= values[backcnt].innerHTML;
-                backcnt++;
-            }
+    if (window.innerWidth >= 321) {
+        cnt = 0;
+        for (let btn of buttons) {
+            btn.innerHTML= values[cnt];
+            cnt++;
         }
     }
 }, false);
